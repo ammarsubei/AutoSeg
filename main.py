ce3296dae7a8ed6e9f5390701c6abf82626e81be
Jeff <JeffKo427@gmail.com>
2017-05-10 10:49:09 -0500

    Moved model building methods to separate file

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import keras
-from keras.models import Model, load_model
-from keras.utils import plot_model, to_categorical
-from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 from keras.engine.topology import Layer
 from keras import backend as K
 import numpy as np
 import sys
+import squeezebuild
 
 num_filters = 64
 img_height = 480
@@ -22,64 +21,8 @@ validation_steps = int(101/batch_size) + 1
 seed = 1
 model_name= sys.argv[1]
 
-def addFireModule(x):
-	squeeze = Conv2D(num_filters, (3,3), padding='same', activation='elu')(x)
-	expand1 = Conv2D(num_filters, (1,1), padding='same', activation='elu')(squeeze)
-	expand3 = Conv2D(num_filters, (3,3), padding='same', activation='elu')(squeeze)
-	c = concatenate([expand1, expand3])
-	
-	return c
-
-def addParallelDilatedConvolution(x):
-	conv1 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=1)(x)
-	conv2 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=2)(x)
-	conv4 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=4)(x)
-	conv8 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=8)(x)
-	c = concatenate([conv1, conv2, conv4, conv8])
-
-	return c
-
-def addBypassRefinementModule(high, low):
-	preConv = Conv2D(num_filters, (3,3), padding='same', activation='elu')(low)
-	c = concatenate([preConv, high])
-	postConv = Conv2D(num_filters, (3,3), padding='same', activation='elu')(c)
-
-	return postConv
-
-def getModel():
-	i = Input(input_shape)
-	convI = Conv2D(num_filters, (3,3), padding='same', activation='elu')(i)
-
-	pool1 = MaxPooling2D((2,2))(convI)
-	fire1_1 = addFireModule(pool1)
-	fire1_2 = addFireModule(fire1_1)
-
-	pool2 = MaxPooling2D((2,2))(fire1_2)
-	fire2_1 = addFireModule(pool2)
-	fire2_2 = addFireModule(fire2_1)
-
-	pool3 = MaxPooling2D((2,2))(fire2_2)
-	fire3_1 = addFireModule(pool3)
-	fire3_2 = addFireModule(fire3_1)
-	fire3_3 = addFireModule(fire3_2)
-	fire3_4 = addFireModule(fire3_3)
-
-	pdc = addParallelDilatedConvolution(fire3_4)
-
-	trans_conv1 = Conv2DTranspose(num_filters, (3,3), padding='same', activation='elu', strides=2)(pdc)
-	ref1 = addBypassRefinementModule(trans_conv1, fire2_2)
-
-	trans_conv2 = Conv2DTranspose(num_filters, (3,3), padding='same', activation='elu', strides=2)(ref1)
-	ref2 = addBypassRefinementModule(trans_conv2, fire1_2)
-
-	trans_conv3 = Conv2DTranspose(num_filters, (3,3), padding='same', activation='elu', strides=2)(ref2)
-	ref3 = addBypassRefinementModule(trans_conv3, convI)
-
-	trans_conv4 = Conv2DTranspose(11, (3,3), padding='same', activation='softmax')(ref3)
-
-	model = Model(inputs=i, outputs=trans_conv4)
-
-	return model
+model = squeezebuild.getModel(input_shape, num_filters)
+model.compile(loss='mean_squared_error', optimizer='adadelta', metrics=['accuracy'])
 
 def zip3(*iterables):
     # zip('ABCD', 'xy') --> Ax By
@@ -154,8 +97,8 @@ tb = TensorBoard(
 
 early = EarlyStopping(patience=batch_size, verbose=1)
 
-model = getModel()
-model.compile(loss='mean_squared_error', optimizer='adadelta', metrics=['accuracy'])
+#sample_img = mpimg.imread('data/validation/images/imgs/0016E5_08151.png')
+
 
 '''model.fit_generator(
 	train_generator,
@@ -172,6 +115,9 @@ for e in range(epochs):
         one_hot = K.one_hot(y_batch, 11)
         y_batch = K.eval(K.squeeze(one_hot, 3))
         model.fit(x_batch, y_batch, batch_size=batch_size)
+        pred = model.predict(sample_img)
+        print(pred.shape)
+        pred_img = np.zeros((img_height,img_width))
         batches += 1
         if batches >= 600 / batch_size:
             # we need to break the loop by hand because
