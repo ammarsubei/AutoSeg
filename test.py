64eb5a3286e4beb3e9b33bff94ee3c1a4598a095
Jeff <JeffKo427@gmail.com>
2017-05-11 14:13:32 -0500

    testing sample image

--- test.py
+++ test.py
@@ -1,24 +1,5 @@
-from keras import backend as K
-from keras.utils import to_categorical
 import numpy as np
-import time
+import cv2, os
 
-test = [[1,2,3],
-		[3,2,1],
-		[1,1,1]]
-test = np.array(test)
-
-start = time.clock()
-K_img = K.eval(K.one_hot(test,4))
-end = time.clock()
-print(end-start)
-
-start = time.clock()
-n_values = np.max(test) + 1
-np_img = np.eye(n_values)[test]
-end = time.clock()
-print(end-start)
-
-recover = np_img.argmax(2)
-
-print(recover)
+img = cv2.imread(os.getcwd() + '/sample_label.png', 0)
+print(np.unique(img))
