b89ce3325c5ef7aabcfcf6c66d75e918e77f540b
Jeff <JeffKo427@gmail.com>
2017-05-12 15:21:15 -0500

    experimentally developed weighted crossentropy pipeline

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-for i in range(10000):
-    print(".",end="")
+import numpy as np
+
+weights = [1,2]
+
+target = np.array([ [[0.0,1.0],[1.0,0.0]],
+                    [[0.0,1.0],[1.0,0.0]]])
+
+output = np.array([ [[0.5,0.5],[0.9,0.1]],
+                    [[0.9,0.1],[0.4,0.6]]])
+
+crossentropy_matrix = -np.sum(target * np.log(output), axis=-1)
+# Multiply each element of crossentropy_matrix by weights[argmax(target)].
+indices = np.argmax(target,2)
+weights_matrix = np.take(weights, indices)
+weighted_crossentropy_matrix = crossentropy_matrix * weights_matrix
+crossentropy = np.sum(weighted_crossentropy_matrix)
+print(crossentropy_matrix)
+print(weights_matrix)
+print(weighted_crossentropy_matrix)
+print(crossentropy)
