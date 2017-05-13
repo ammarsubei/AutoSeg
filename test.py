a32b9eeffd19fbfa27b8d46b1e446e592416bd0b
Jeff <JeffKo427@gmail.com>
2017-05-12 19:59:21 -0500

    distribute model parameters more efficiently

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ output = np.array([ [[0.5,0.5],[0.9,0.1]],
                     [[0.9,0.1],[0.4,0.6]]])
 
 crossentropy_matrix = -np.sum(target * np.log(output), axis=-1)
+unweighted_crossentropy = -np.sum(target * np.log(output))
 # Multiply each element of crossentropy_matrix by weights[argmax(target)].
 indices = np.argmax(target,2)
 weights_matrix = np.take(weights, indices)
