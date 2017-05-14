f778e8c00b9497a7d2f883a5ff2960e4dfc49cb0
Jeff <JeffKo427@gmail.com>
2017-05-14 10:32:14 -0500

    change layer names

--- autoseg_models.py
+++ autoseg_models.py
@@ -1,10 +1,10 @@
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Reshape, concatenate, add
 
-def addFireModule(x, squeeze_filters, expand_filters):
-    squeeze = Conv2D(16, (3,3), padding='same', activation='elu')(x)
-    expand1 = Conv2D(expand_filters, (1,1), padding='same', activation='elu')(squeeze)
-    expand3 = Conv2D(expand_filters, (3,3), padding='same', activation='elu')(squeeze)
+def addFireModule(x, squeeze_filters, expand_filters, name='fire'):
+    squeeze = Conv2D(16, (3,3), padding='same', activation='elu', name=name + '/squeeze1x1')(x)
+    expand1 = Conv2D(expand_filters, (1,1), padding='same', activation='elu', name=name + '/expand1x1')(squeeze)
+    expand3 = Conv2D(expand_filters, (3,3), padding='same', activation='elu', name=name + '/expand3x3')(squeeze)
     c = concatenate([expand1, expand3])
 
     return c
@@ -27,21 +27,21 @@ def addBypassRefinementModule(high, low, num_filters):
 
 def getModel(input_shape, num_classes, num_filters):
     i = Input(input_shape)
-    convI = Conv2D(96, (3,3), padding='same', activation='elu')(i)
+    convI = Conv2D(96, (3,3), padding='same', activation='elu', name='conv1')(i)
 
     pool1 = MaxPooling2D((2,2))(convI)
-    fire1_1 = addFireModule(pool1, 16, 64)
-    fire1_2 = addFireModule(fire1_1, 16, 64)
+    fire1_1 = addFireModule(pool1, 16, 64, name='fire2')
+    fire1_2 = addFireModule(fire1_1, 16, 64, name='fire3')
 
     pool2 = MaxPooling2D((2,2))(fire1_2)
-    fire2_1 = addFireModule(pool2, 32, 128)
-    fire2_2 = addFireModule(fire2_1, 32, 128)
+    fire2_1 = addFireModule(pool2, 32, 128, name='fire4')
+    fire2_2 = addFireModule(fire2_1, 32, 128, name='fire5')
 
     pool3 = MaxPooling2D((2,2))(fire2_2)
-    fire3_1 = addFireModule(pool3, 48, 192)
-    fire3_2 = addFireModule(fire3_1, 48, 192)
-    fire3_3 = addFireModule(fire3_2, 64, 256)
-    fire3_4 = addFireModule(fire3_3, 64, 256)
+    fire3_1 = addFireModule(pool3, 48, 192, name='fire6')
+    fire3_2 = addFireModule(fire3_1, 48, 192, name='fire7')
+    fire3_3 = addFireModule(fire3_2, 64, 256, name='fire8')
+    fire3_4 = addFireModule(fire3_3, 64, 256, name='fire9')
 
     pdc = addParallelDilatedConvolution(fire3_4, 512)
 
