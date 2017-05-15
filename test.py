53af5934a2f1001070e7bc61af3351c35965529e
Jeff <JeffKo427@gmail.com>
2017-05-15 16:43:31 -0500

    test stuff

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-import os
+import numpy as np
+import cv2
 
-data_dir = '../../color'
-
-allfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
-
-for f in allfiles:
-    f = f[:27]
-    f = f.split('_')[0] + '/' + f
-    print(f)
-
-training_file_list = []
-for f in allfiles:
-    self.training_file_list.append( ('leftImg8bit/train/' + f + '_leftImg8bit.png', 'gtCoarse/train/' + f + '_gtCoarse_labelIds.png') )
+label = cv2.imread('../../labels/munich_000188_000019_gtFine_labelIds.png')
+print(np.unique(label))
