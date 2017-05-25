dd121a03605cacb2d27a94ea79fc95ba0e19370b
Jeff <JeffKo427@gmail.com>
2017-05-25 18:42:38 -0400

    one more time

--- autoseg_backend.py
+++ autoseg_backend.py
@@ -25,15 +25,30 @@ def oneHotToLabel(one_hot):
 def remapClass(arr):
     arr[arr<=6] = 0
     arr[arr==7] = 1
+
     arr[arr==8] = 2
     arr[arr==9] = 2
     arr[arr==10] = 2
-    arr[arr>=11 and arr<=16] = 3
-    arr[arr>=17 and arr<=20] = 4
+
+    arr[arr==11] = 3
+    arr[arr==12] = 3
+    arr[arr==13] = 3
+    arr[arr==14] = 3
+    arr[arr==15] = 3
+    arr[arr==16] = 3
+
+    arr[arr==17] = 4
+    arr[arr==18] = 4
+    arr[arr==19] = 4
+    arr[arr==20] = 4
+
     arr[arr==21] = 5
     arr[arr==22] = 6
     arr[arr==23] = 7
-    arr[arr==24 or arr==25] = 8
+
+    arr[arr==24] = 8
+    arr[arr==25] = 8
+    
     arr[arr>=26] = 9
 
 def getID(size=6, chars=string.ascii_lowercase + string.digits):
