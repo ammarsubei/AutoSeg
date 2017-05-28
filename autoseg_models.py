5bf6e53f1ec654a477116f7ee732f9f0618d2dbd
Jeff <JeffKo427@gmail.com>
2017-05-28 09:01:33 -0400

    implemented weight decay

--- autoseg_models.py
+++ autoseg_models.py
@@ -1,33 +1,34 @@
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Reshape, Dropout, concatenate, add
+from keras.regularizers import l2
 
 def addFireModule(x, squeeze_filters, expand_filters, name='fire'):
-    squeeze = Conv2D(squeeze_filters, (1,1), padding='same', activation='elu', name=name + '/squeeze1x1')(x)
-    expand1 = Conv2D(expand_filters, (1,1), padding='same', activation='elu', name=name + '/expand1x1')(squeeze)
-    expand3 = Conv2D(expand_filters, (3,3), padding='same', activation='elu', name=name + '/expand3x3')(squeeze)
+    squeeze = Conv2D(squeeze_filters, (1,1), padding='same', activation='elu', name=name + '/squeeze1x1', kernel_regularizer=l2(0.0002))(x)
+    expand1 = Conv2D(expand_filters, (1,1), padding='same', activation='elu', name=name + '/expand1x1', kernel_regularizer=l2(0.0002))(squeeze)
+    expand3 = Conv2D(expand_filters, (3,3), padding='same', activation='elu', name=name + '/expand3x3', kernel_regularizer=l2(0.0002))(squeeze)
     c = concatenate([expand1, expand3])
 
     return c
 
 def addParallelDilatedConvolution(x, num_filters, name='parallel_dilated_convolution'):
-    conv1 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=1, name=name + '/dil_1')(x)
-    conv2 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=2, name=name + '/dil_2')(x)
-    conv4 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=4, name=name + '/dil_4')(x)
-    conv8 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=8, name=name + '/dil_8')(x)
+    conv1 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=1, name=name + '/dil_1', kernel_regularizer=l2(0.0002))(x)
+    conv2 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=2, name=name + '/dil_2', kernel_regularizer=l2(0.0002))(x)
+    conv4 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=4, name=name + '/dil_4', kernel_regularizer=l2(0.0002))(x)
+    conv8 = Conv2D(num_filters, (3,3), padding='same', activation='elu', dilation_rate=8, name=name + '/dil_8', kernel_regularizer=l2(0.0002))(x)
     a = add([conv1, conv2, conv4, conv8])
 
     return a
 
 def addBypassRefinementModule(high, low, num_filters, name='bypass', dropout_rate=0.2):
-    preConv = Conv2D(num_filters, (3,3), padding='same', activation='elu', name=name + '/pre_conv')(low)
+    preConv = Conv2D(num_filters, (3,3), padding='same', activation='elu', name=name + '/pre_conv', kernel_regularizer=l2(0.0002))(low)
     c = concatenate([preConv, high])
-    postConv = Conv2D(num_filters, (3,3), padding='same', activation='elu', name=name + '/post_conv')(c)
+    postConv = Conv2D(num_filters, (3,3), padding='same', activation='elu', name=name + '/post_conv', kernel_regularizer=l2(0.0002))(c)
 
     return Dropout(dropout_rate)(postConv)
 
 def getModel(input_shape, num_classes, residual_encoder_connections=False, dropout_rate=0.2):
     i = Input(input_shape)
-    convI = Conv2D(64, (3,3), padding='same', activation='elu', name='conv1')(i)
+    convI = Conv2D(64, (3,3), padding='same', activation='elu', name='conv1', kernel_regularizer=l2(0.0002))(i)
 
     if residual_encoder_connections:
         pool1 = MaxPooling2D(2)( concatenate( [convI, i] ) )
@@ -60,15 +61,15 @@ def getModel(input_shape, num_classes, residual_encoder_connections=False, dropo
     pdc = addParallelDilatedConvolution(pool4, 512, name='parallel_dilated_convolution')
 
     ref10 = addBypassRefinementModule(pdc, pool3, 256, name='bypass10')
-    trans_conv11 = Conv2DTranspose(256, (3,3), padding='same', activation='elu', strides=2, name='trans_conv11')(Dropout(dropout_rate)(ref10))
+    trans_conv11 = Conv2DTranspose(256, (3,3), padding='same', activation='elu', strides=2, name='trans_conv11', kernel_regularizer=l2(0.0002))(Dropout(dropout_rate)(ref10))
 
     ref12 = addBypassRefinementModule(trans_conv11, pool2, 128, name='bypass12')
-    trans_conv13 = Conv2DTranspose(128, (3,3), padding='same', activation='elu', strides=2, name='trans_conv13')(Dropout(dropout_rate)(ref12))
+    trans_conv13 = Conv2DTranspose(128, (3,3), padding='same', activation='elu', strides=2, name='trans_conv13', kernel_regularizer=l2(0.0002))(Dropout(dropout_rate)(ref12))
 
     ref14 = addBypassRefinementModule(trans_conv13, pool1, 64, name='bypass14', dropout_rate=dropout_rate)
-    trans_conv15 = Conv2DTranspose(64, (3,3), padding='same', activation='elu', strides=2, name='trans_conv15')(Dropout(dropout_rate)(ref14))
+    trans_conv15 = Conv2DTranspose(64, (3,3), padding='same', activation='elu', strides=2, name='trans_conv15', kernel_regularizer=l2(0.0002))(Dropout(dropout_rate)(ref14))
 
-    prediction = Conv2D(num_classes, (1,1), padding='same', activation='softmax', name='main')(trans_conv15)
+    prediction = Conv2D(num_classes, (1,1), padding='same', activation='softmax', name='main', kernel_regularizer=l2(0.0002))(trans_conv15)
 
     model = Model(inputs=i, outputs=prediction)
 
--- test.py
+++ test.py
@@ -1,34 +1,36 @@
-<<<<<<< HEAD
-import cv2, time, os
-
-image = cv2.imread('./cityscapes/images/val/munster/munster_000097_000019_leftImg8bit.png')
-label = cv2.imread('./cityscapes/labels_fine/val/munster/munster_000097_000019_gt_Fine_labelIds.png')
-start = time.clock()
-image = cv2.resize(image, (480,240))
-label = cv2.resize(image, (480,240))
-end = time.clock()
-print(2500*(end - start))
-=======
 import numpy as np
-import cv2, random
 
-<<<<<<< HEAD
-img = cv2.imread('../../img.jpg')
-img = cv2.resize(img, (800,600))
-rows,cols = img.shape[:2]
-while True:
-    angle = random.gauss(mu=0, sigma=4)
-    print(angle)
-    scale = random.gauss(mu=1, sigma=0.1)
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,scale)
-    dst = cv2.warpAffine(img,M,(cols,rows))
-    cv2.imshow('Original', img)
-    cv2.imshow('Rotated', dst)
-    cv2.moveWindow('Original', 10,10)
-    cv2.moveWindow('Rotated', 700,10)
-    cv2.waitKey(1000)
-=======
-label = cv2.imread('../../labels/munich_000188_000019_gtFine_labelIds.png')
-print(np.unique(label))
->>>>>>> 7d49b792332f3f8179a95829e2830a107014cb84
->>>>>>> c14cca7c74e9df13616785f846ef1b531e51d7d9
+# Reduces the 34 CityScapes classes to 9.
+def remapClass(arr):
+    arr[arr<=6] = 0
+    arr[arr==7] = 1
+
+    arr[arr==8] = 2
+    arr[arr==9] = 2
+    arr[arr==10] = 2
+
+    arr[arr==11] = 3
+    arr[arr==12] = 3
+    arr[arr==13] = 3
+    arr[arr==14] = 3
+    arr[arr==15] = 3
+    arr[arr==16] = 3
+
+    arr[arr==17] = 4
+    arr[arr==18] = 4
+    arr[arr==19] = 4
+    arr[arr==20] = 4
+
+    arr[arr==21] = 5
+    arr[arr==22] = 6
+    arr[arr==23] = 7
+
+    arr[arr==24] = 8
+    arr[arr==25] = 8
+    
+    arr[arr>=26] = 9
+
+arr = np.array([[1,2],[23,24]])
+print(arr)
+remapClass(arr)
+print(arr)
