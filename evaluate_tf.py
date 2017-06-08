5675cbc6baace543b576b3bf1fb142879c3f7602
Jeff <JeffKo427@gmail.com>
2017-06-08 16:06:04 -0500

    created script for running model in tensorflow

---
+++ evaluate_tf.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import cv2
+from autoseg_backend import BackendHandler
+
+num_classes = 34
+data_dir = '/cityscapes_1024/'
+
+sess = tf.Session()
+saver = tf.train.import_meta_graph('my_test_model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+backend = BackendHandler(data_dir=data_dir, num_classes=num_classes, visualize_while_training=visualize_while_training)
+
+for batch in backend.generateData(batch_size=3):
+    predictions = main.eval(feed_dict={
+        input_1: batch})
+    for i in range(len(predictions)):
+        ID = getID()
+        cv2.imshow('Image', x[i])
+        cv2.moveWindow('Image', 10, 10)
+        cv2.imshow('Ground Truth', makeLabelPretty( oneHotToLabel(y[i]) ))
+        cv2.moveWindow('Ground Truth', 850, 10)
+        cv2.imshow('Model Output', makeLabelPretty( oneHotToLabel(predictions[i]) ))
+        cv2.moveWindow('Model Output', 850, 500)
+        cv2.waitKey(5000)
