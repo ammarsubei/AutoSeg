37556e8eb8d9a8e543abfaf13a7d97794f99a079
Jeff Alan <JeffKo427@gmail.com>
2017-06-12 11:32:50 -0500

    updates

--- record_ros_data.py
+++ record_ros_data.py
@@ -2,20 +2,25 @@
 import rospy
 from sensor_msgs.msg import Image
 from can_translator.msg import Steering
-import pickle
+import pickle, string, random
 
 image = None
+previous_image = None
 steering = None
 
 def image_callback(data):
+    global image
     image = data.data
+    #print("Got image!")
 
 def angle_callback(data):
+    global steering
     steering = data.steering_angle
+    #print("Got steering!")
 
 def getID(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-    
+
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -28,16 +33,21 @@ def listener():
     rospy.Subscriber("/camera/left/image_raw", Image, image_callback)
     rospy.Subscriber("/vehicle/steering", Steering, angle_callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spinOnce()
-    rospy.sleep(0.2)
-
     # write image and steering angle to pickle file
-    if image is not None and steering is not None:
-        xy = (image, steering)
-        ID = getID()
-        with open('rosbag_data/' + ID, 'wb') as f:
-            pickle.dump(xy, f, protocol=pickle.HIGHEST_PROTOCOL)
+    global previous_image
+    while True:
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.sleep(0.2)
+        if image is not None and steering is not None:
+            xy = (image, steering)
+            ID = getID()
+            with open('rosbag_data/' + ID, 'wb') as f:
+                pickle.dump(xy, f, protocol=pickle.HIGHEST_PROTOCOL)
+                print(ID)
+            if image == previous_image:
+                break
+            else:
+                previous_image = image
 
 if __name__ == '__main__':
     listener()
