26d82ba6558771b8b05bcb9362dbebe4fcbc9aca
Jeff Alan <JeffKo427@gmail.com>
2017-06-12 15:31:45 -0500

    successfully ran keras-trained model in raw tensorflow

--- evaluate_tf.py
+++ evaluate_tf.py
@@ -1,31 +1,44 @@
 import tensorflow as tf
 import cv2
-import os
-from autoseg_backend import BackendHandler
+import os, pickle
+import numpy as np
+from autoseg_backend import BackendHandler, oneHotToLabel
 
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 num_classes = 34
-data_dir = '/cityscapes_1024/'
+data_dir = '/cityscapes_800/'
+
+def makeLabelPretty(label):
+    prettyLabel = cv2.cvtColor(label, cv2.COLOR_GRAY2RGB)
+    with open('cityscapes_color_mappings.pickle', 'rb') as f:
+        colors = pickle.load(f)
+    for i in range(num_classes):
+        prettyLabel[np.where( (label==[i]) )] = colors[i]
+
+    return prettyLabel
 
 sess = tf.Session()
 saver = tf.train.import_meta_graph('my_test_model.meta')
 saver.restore(sess, tf.train.latest_checkpoint('./'))
+print(sess.graph.get_operations())
 
-last_op = sess.graph.get_operation_by_name('main/Sum_1')
+last_op = sess.graph.get_operation_by_name('main/truediv')
 main = last_op.values()[0]
-print(main)
 
 backend = BackendHandler(data_dir=data_dir, num_classes=num_classes)
 
-for batch in backend.generateData(batch_size=3, validating=True):
+x = sess.graph.get_tensor_by_name('input_1:0')
+d1 = sess.graph.get_tensor_by_name('dropout_1/keras_learning_phase:0')
+
+for batch in backend.generateData(batch_size=1, validating=True):
+    print(batch[0].shape)
     predictions = main.eval(feed_dict={
-        input_1: batch})
+        x: batch[0], d1: False}, session=sess)
     for i in range(len(predictions)):
-        ID = getID()
-        cv2.imshow('Image', x[i])
+        cv2.imshow('Image', batch[0][i])
         cv2.moveWindow('Image', 10, 10)
-        cv2.imshow('Ground Truth', makeLabelPretty( oneHotToLabel( y[i]) ))
+        cv2.imshow('Ground Truth', makeLabelPretty( oneHotToLabel( batch[1][i] ) ))
         cv2.moveWindow('Ground Truth', 850, 10)
         cv2.imshow('Model Output', makeLabelPretty( oneHotToLabel(predictions[i]) ))
         cv2.moveWindow('Model Output', 850, 500)
