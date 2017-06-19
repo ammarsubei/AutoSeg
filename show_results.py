ffc2c90ef7b7519319253158f1312e7bca146061
Jeff Alan <JeffKo427@gmail.com>
2017-06-19 18:11:04 -0500

    misc changes

--- show_results.py
+++ show_results.py
@@ -13,16 +13,16 @@ os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 train_encoder = True
 num_classes = 34
-data_dir = '/cityscapes_1024/'
-img_height = 512
-img_width = 1024
+data_dir = '/cityscapes_480/'
+img_height = 240
+img_width = 1480
 visualize_while_training = True
 dropout_rate = 0.4
 weight_decay=0.0002
 img_size = (img_width, img_height)
 mask_size = img_size
 input_shape = (img_height, img_width, 3)
-batch_size = 3
+batch_size = 1
 epochs = 10000000
 model_name= 'visualized_model.h5'
 
