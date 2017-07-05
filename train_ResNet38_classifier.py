1400b9240741d41a564773946d24a68af60aca99
Jeff Alan <JeffKo427@gmail.com>
2017-07-05 11:31:44 -0500

    create script for training RN38 classifier

---
+++ train_ResNet38_classifier.py
@@ -0,0 +1,51 @@
+import keras
+from keras import backend as K
+from keras.models import load_model
+from keras.utils import plot_model
+import tensorflow as tf
+import numpy as np
+import os, sys, time
+import autoseg_models
+from autoseg_backend import BackendHandler, pixelwise_crossentropy, class_weighted_pixelwise_crossentropy, pixelwise_accuracy
+from keras.preprocessing.image import ImageDataGenerator
+
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
+train_encoder = True
+num_classes = 34
+data_dir = '/cityscapes_400/'
+img_height = 200
+img_width = 400
+visualize_while_training = False
+dropout_rate = 0.4
+weight_decay=0.0002
+img_size = (img_width, img_height)
+input_shape = (img_height, img_width, 3)
+batch_size = 3
+epochs = 10000000
+if len(sys.argv) > 1:
+    model_name = sys.argv[1]
+else:
+    model_name= 'ResNet38.h5'
+
+model = autoseg_models.get_rn38_classifier(input_shape=input_shape,
+                                num_classes=num_classes)
+
+if model_name in os.listdir(os.getcwd()):
+    model.load_weights(model_name, by_name=True)
+
+sgd = keras.optimizers.SGD(lr=1e-8, momentum=0.9, decay=1e-6)
+model.compile(loss=pixelwise_crossentropy, optimizer=sgd, metrics=[pixelwise_accuracy])
+plot_model(model, to_file='architecture.png', show_shapes=True, show_layer_names=True)
+
+backend = BackendHandler(data_dir=data_dir, num_classes=num_classes, visualize_while_training=visualize_while_training)
+callbacks = backend.get_callbacks(model_name, patience=250, logdir='./logs/ResNet38/')
+
+model.fit_generator(
+    backend.generate_data(batch_size),
+    steps_per_epoch=200, #len(backend.training_file_list) // batch_size,
+    epochs=epochs,
+    callbacks=callbacks,
+    validation_data=backend.generate_data(batch_size, validating=True),
+    validation_steps=len(backend.validation_file_list) // batch_size)
+    #class_weight=backend.class_weights)
