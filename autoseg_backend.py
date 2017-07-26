1152cfb0b01c4cca5184bad8a6c9193d27e755f8
Jeff <JeffKo427@gmail.com>
2017-07-26 10:51:12 -0500

    finished refactoring code

--- autoseg_backend.py
+++ autoseg_backend.py
@@ -1,11 +1,9 @@
+import os
+
 from keras.callbacks import TensorBoard, ModelCheckpoint
 from keras import backend as K
 import tensorflow as tf
-import os, random, math, string
-import pickle
-import numpy as np
 import cv2
-from enum import Enum
 
 class Dataset(object):
     """
@@ -16,55 +14,50 @@ class Dataset(object):
                  validation_input_dirs, validation_output_dirs):
         self.name = name
         self.num_classes = num_classes
-        training_inputs = self.get_file_list(training_input_dirs)
-        training_outputs = self.get_file_list(training_output_dirs)
-        validation_inputs = self.get_file_list(validation_input_dirs)
-        validation_outputs = self.get_file_list(validation_output_dirs)
+        training_inputs = get_file_list(training_input_dirs)
+        training_outputs = get_file_list(training_output_dirs)
+        validation_inputs = get_file_list(validation_input_dirs)
+        validation_outputs = get_file_list(validation_output_dirs)
         self.training_data = (training_inputs, training_outputs)
         self.validation_data = (validation_inputs, validation_outputs)
 
-    def get_file_list(self, directories):
-        """
-        For each directory in a list, recursively find all files in it.
-        Return a list of lists of files of the same length as directories.
-        """
-        file_list = []
-        for directory in directories:
-            contents = []
-            for path, subdirs, files in os.walk(directory):
-                for f in files:
-                    contents.append(os.path.join(path, f))
-            contents.sort()
-            file_list.append(contents)
+def get_file_list(directories):
+    """
+    For each directory in a list, recursively find all files in it.
+    Return a list of lists of files of the same length as directories.
+    """
+    file_list = []
+    for directory in directories:
+        contents = []
+        for path, subdirs, files in os.walk(directory):
+            for f in files:
+                contents.append(os.path.join(path, f))
+        contents.sort()
+        file_list.append(contents)
 
-        return file_list
-
-def label_to_onehot(label, num_classes):
-    """Converts labels (e.g. 2) to one-hot vectors (e.g. [0,0,1,0]).
-    Accepts and returns a numpy array."""
-    return np.eye(num_classes)[label]
-
-# Accepts and returns a numpy array.
-def one_hot_to_label(one_hot):
-    """Converts one-hot vectors (e.g. [0,0,1,0]) to labels (e.g. 2).
-    Accepts and returns a numpy array."""
-    return one_hot.argmax(2).astype('uint8')
+    return file_list
 
 def pixelwise_crossentropy(target, output):
-    """Keras' default crossentropy function wasn't working, not sure why."""
+    """
+    Keras' default crossentropy function wasn't working, not sure why.
+    """
     output = tf.clip_by_value(output, 10e-8, 1.-10e-8)
-    return -tf.reduce_sum(target * IGNORE_CLASSES * tf.log(output))
+    return -tf.reduce_sum(target * tf.log(output))
 
 def pixelwise_accuracy(y_true, y_pred):
-    """Same as Keras' default accuracy function, but with axis=-1 changed to axis=2.
-        These should be equivalent, I'm not sure why they result in different values."""
+    """
+    Same as Keras' default accuracy function, but with axis=-1 changed to axis=2.
+    These should be equivalent, I'm not sure why they result in different values.
+    """
     return K.mean(K.cast(K.equal(K.argmax(y_true, axis=2),
-                         K.argmax(y_pred, axis=2)),
+                                 K.argmax(y_pred, axis=2)),
                          K.floatx()))
 
-def get_callbacks(self, model_name='test.h5', patience=500, logdir='./logs/default'):
-    """Returns a standard set of callbacks.
-        Kept here mainly to avoid clutter in main.py"""
+def get_callbacks(model_name='test.h5', patience=500, logdir='./logs/default'):
+    """
+    Returns a standard set of callbacks.
+    Kept here mainly to avoid clutter in train_model.py
+    """
     checkpoint = ModelCheckpoint(
         model_name,
         monitor='val_loss',
@@ -78,7 +71,5 @@ def get_callbacks(self, model_name='test.h5', patience=500, logdir='./logs/defau
         write_graph=True,
         write_grads=True,
         write_images=True)
-
-    early = EarlyStopping(monitor='val_loss', patience=patience, verbose=1)
 
     return [checkpoint, tb]
--- autoseg_datagen.py
+++ autoseg_datagen.py
@@ -1,78 +1,47 @@
 """Module docstring"""
 
-import os
-import numpy as np
 import random
+import numpy as np
 import cv2
 
-def augment_data(horizontal_flip=True, vertical_flip=False,
-                 adjust_brightness=0.1, rotate=5, zoom=0.1):
-    """Data Augmentation."""
-    if not validating:
-        # Horizontal flip.
-        if horizontal_flip and random.randint(0, 1):
-            cv2.flip(image, 1)
-            cv2.flip(label, 1)
-        # Vertical flip.
-        if vertical_flip and random.randint(0, 1):
-            cv2.flip(image, 0)
-            cv2.flip(label, 0)
-        # Randomly make image brighter or darker.
-        if adjust_brightness:
-            factor = 1 + abs(random.gauss(mu=0, sigma=adjust_brightness))
-            if random.randint(0, 1):
-                factor = 1 / factor
-            image = 255*((image/255)**factor)
-            image = np.array(image, dtype='uint8')
-        # Randomly rotate image within a certain range.
-        if rotate:
-            angle = random.gauss(mu=0, sigma=rotate)
-        else:
-            angle = 0
-        # Randomly zoom image.
-        if zoom:
-            scale = random.gauss(mu=1, sigma=zoom)
-        else:
-            scale = 1
-        # Apply rotation/zoom calculated above.
-        if rotate or zoom:
-            rows, cols = label.shape
-            M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, scale)
-            image = cv2.warpAffine(image, M, (cols, rows))
-            label = cv2.warpAffine(label, M, (cols, rows))
+def label_to_onehot(label, num_classes):
+    """Converts labels (e.g. 2) to one-hot vectors (e.g. [0,0,1,0]).
+    Accepts and returns a numpy array."""
+    return np.eye(num_classes)[label]
 
-def generate_data(inputs, outputs, batch_size):
+# Accepts and returns a numpy array.
+def one_hot_to_label(one_hot):
+    """Converts one-hot vectors (e.g. [0,0,1,0]) to labels (e.g. 2).
+    Accepts and returns a numpy array."""
+    return one_hot.argmax(2).astype('uint8')
+
+def generate_data(data, batch_size, num_classes, augment_data=False):
     """Replaces Keras' native ImageDataGenerator."""
-    if validating:
-        data = self.validation_file_list
-    else:
-        data = self.training_file_list
     random.shuffle(data)
 
     i = 0
     while True:
-        image_batch = []
-        image_batch_right = []
-        label_batch = []
-        for batch in range(batch_size):
+        input_batch = []
+        output_batch = []
+        for b in range(batch_size):
             if i == len(data):
                 i = 0
                 random.shuffle(data)
             sample = data[i]
             i += 1
-            image = cv2.imread(sample[0])
-            image_right = cv2.imread(sample[1])
-            label = remap_class(cv2.imread(sample[2], 0))
 
-            '''
+            inputs = []
+            for inp in sample[0]:
+                inputs.append((cv2.imread(inp).astype(float) - 128) / 128)
+
+            outputs = []
+            for outp in sample[1]:
+                outputs.append(label_to_onehot(cv2.imread(outp, 0), num_classes))
+
+            input_batch.append(inputs)
+            output_batch.append(outputs)
 
 
-            '''
-            one_hot = label_to_onehot(label, self.num_classes)
-            image_batch.append((image.astype(float) - 128) / 128)
-            image_batch_right.append((image_right.astype(float) - 128) / 128)
-            label_batch.append(one_hot)
-        image_batch = np.array(image_batch)
-        image_batch_right = np.array(image_batch_right)
-        label_batch = np.array(label_batch)
-        yield ([image_batch, image_batch_right], label_batch)
+        input_batch = np.array(input_batch)
+        output_batch = np.array(output_batch)
+        yield (input_batch, output_batch)
