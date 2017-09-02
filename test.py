c267fd0994b05a2ed9b4cdef077580e4e0290a56
almond <ammarsubei@gmail.com>
2017-09-01 20:01:21 -0500

    Removes unneeded files

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-pretty = [[[1, 1, 1], [2, 2, 2]],
-          [[2, 2, 2], [2, 2, 2]]]
-pretty = np.array(pretty)
-print(pretty.shape)
-label = np.zeros((2,2,1))
-print(label.shape)
-print([1, 1, 1] == [1, 1, 0])
